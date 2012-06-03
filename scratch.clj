71b99c7908219da92eae7f3f7034397b21167ad1
nick rothwell <nick@cassiel.com>
2012-06-03 23:35:37 +0100

    Tests.

--- scratch.clj
+++ scratch.clj
@@ -1,6 +1,27 @@
 (ns user
-  (:require (cell-sequencer [core :as c])))
+  (:require (cell-sequencer [core :as c]
+                            [max-io :as io])))
 
 (nil? (re-find #"^X." "XaXbXc"))
 
 (not (nil? (re-find #"^-?\d" "-23")))
+
+(eval (io/parse-event "inc 5"))
+
+(fn? (eval (io/parse-event "fn [x] x")))
+
+(fn? (fn [x] x))
+
+(eval (io/parse-event "56"))
+
+(class (io/parse-event "3 4"))
+
+(class (io/parse-event "inc"))
+
+(defn doit [x y text]
+  (let [v (eval (io/parse-event text))]
+    (if (fn? v)
+      (v x y)
+      v)))
+
+(doit 2 3 "identity")
